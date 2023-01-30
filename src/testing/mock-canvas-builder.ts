000847eac4a0ee3df20301aed9e8c5dbc97a959a
Cariad Eccleston <cariad@cariad.earth>
2023-01-30 08:15:06 +0000

    Rename Point to Vector

--- src/testing/mock-canvas-builder.ts
+++ src/testing/mock-canvas-builder.ts
@@ -4,7 +4,7 @@ import { Image } from 'canvas';
 
 import ICanvasBuilder from '../interfaces/canvas-builder';
 import MakeCanvasBuilder from '../types/make-canvas-builder';
-import Point from '../types/point';
+import Vector from '../types/vector';
 import Rectangle from '../types/rectangle';
 
 export default class MockCanvasBuilder implements ICanvasBuilder {
@@ -20,12 +20,12 @@ export default class MockCanvasBuilder implements ICanvasBuilder {
     return [makeCanvasBuilder, canvasBuilder];
   }
 
-  public drawImage(image: Image, source: Rectangle, at: Point): ICanvasBuilder {
+  public drawImage(image: Image, source: Rectangle, at: Vector): ICanvasBuilder {
     this.log.push(`drawImage: source=${source.toString()} at=${at.toString()}`);
     return this;
   }
 
-  public drawText(text: string, at: Point): ICanvasBuilder {
+  public drawText(text: string, at: Vector): ICanvasBuilder {
     this.log.push(`drawText: text=${text} at=${at.toString()}`);
     return this;
   }
