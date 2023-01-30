9ec47c40ab80643d8e7e6d4bcb5250513e9c6fd9
Cariad Eccleston <cariad@cariad.earth>
2023-01-30 08:14:50 +0000

    Add data property

--- src/interfaces/surface.ts
+++ src/interfaces/surface.ts
@@ -1,4 +1,8 @@
+import ISurfaceSchema from './schemas/surface';
+import Vector from '../types/vector';
+
 export default interface ISurface {
+  data: ISurfaceSchema;
   name: string;
-  size: [number, number];
+  size: Vector;
 }
